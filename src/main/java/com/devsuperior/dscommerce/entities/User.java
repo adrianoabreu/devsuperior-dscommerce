0b4e319f4d85ecbaafc4ef49220054f70b9b5791
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2025-01-23 07:39:36 -0300

    Adicionando seguranca ao projeto DSCommerce atraves do modelo de conceitual User-Role e incrementando seed do banco de dados

--- src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -2,14 +2,18 @@ package com.devsuperior.dscommerce.entities;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
-
+import java.util.Set;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
@@ -30,6 +34,12 @@ public class User {
 	
 	@OneToMany(mappedBy = "client")
 	private List<Order> orders = new ArrayList<>();
+	
+    @ManyToMany
+    @JoinTable(name = "tb_user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
 	
 	public User() {
 
@@ -93,12 +103,26 @@ public class User {
 		this.password = password;
 	}
 
-	
-	
 	public List<Order> getOrders() {
 		return orders;
 	}
 
+    public void addRole(Role role) {
+    	roles.add(role);
+    }
+    
+    /*Metodo que retorna verdadeiro ou falso para nome de perfil(role) de usuário informado.
+      A ideia é saber se o usuário possui o perfil informado ou não.
+    */
+    public boolean hasRole(String roleName) {
+    	for(Role role : roles) {
+    		if(role.getAuthority().equals(roleName)) {
+    			return true;
+    		}
+    	}
+    	return false;
+    }
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
